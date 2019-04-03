35c9a0bb4e338c1d889474c3feaeee8581b6cfa3
t826657 <t826657@spark.co.nz>
2019-04-03 14:42:57 +1300

    Node

---
+++ node-app-spark2/server.js
@@ -0,0 +1,35 @@
+//npm install pg@6
+//npm install express
+const pg        = require('pg');
+const express   = require('express');
+const app       = express();
+
+const config = {
+    user: 'postgres',
+    database: 'TestDb',
+    password: 'idpsparkdb',
+    port: 5432,
+	host: 'capwint03'
+};
+
+// pool takes the object above -config- as parameter
+const pool = new pg.Pool(config);
+
+app.get('/', (req, res, next) => {
+   pool.connect(function (err, client, done) {
+       if (err) {
+           console.log("Can not connect to the DB" + err);
+       }
+       client.query('SELECT * FROM users', function (err, result) {
+            done();
+            if (err) {
+                console.log(err);
+                res.status(400).send(err);
+            }
+            res.status(200).send(result.rows);
+       })
+   })
+});
+app.listen(4000, function () {
+    console.log('Server is running.. on Port 4000');
+});
